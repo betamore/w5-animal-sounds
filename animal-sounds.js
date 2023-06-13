067aa402ca4c4dadb94a3ba68425ec8218a4e5e9
Aaron Brooks <abrooks@mastermnd.io>
2023-06-13 19:35:35 -0400

    animal-sounds project created

---
+++ animal-sounds.js
@@ -0,0 +1,10 @@
+// Write code here
+
+
+
+// DONT CHANGE. THIS IS FOR TESTING
+let animals = [ "Bat", "Cat", "Cat", "Dog", "Cat", "Duck", "Leopard", "Fish"]
+
+for (let animals of animals) {
+	console.log(whatSound(animal)
+}
